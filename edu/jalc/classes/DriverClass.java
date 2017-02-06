f455e16c7292f3589f7757031b9c2cc196f38c66
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-06 08:50:24 -0600

    classes 2/6

---
+++ edu/jalc/classes/DriverClass.java
@@ -0,0 +1,8 @@
+package edu.jalc.classes;
+
+public class DriverClass{
+  public static void main(String[] args){
+    UselessClass uselessClass = new UselessClass();
+
+  }
+}
---
+++ edu/jalc/classes/UselessClass.java
@@ -0,0 +1,11 @@
+package edu.jalc.classes;
+
+public class UselessClass{
+  public static void main(String[] args){
+
+  }
+}
+//4)public- everyone
+//1) private- class itself only
+//3)protected- package and subclasses
+//2)[package private]-defualt, only the package can access it
