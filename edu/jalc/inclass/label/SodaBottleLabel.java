b521c37552e8d8fcfe0d215d73b6f6dd718fa287
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-09 08:48:38 -0600

    label not finished

--- edu/jalc/inclass/label/SodaBottleLabel.java
+++ edu/jalc/inclass/label/SodaBottleLabel.java
@@ -1,12 +1,22 @@
 package edu.jalc.inclass.label;
 
 public class SodaBottleLabel{
-  int height;
+  double height;
+  //needs composit object
+   Logo logo;
+   Color color;
 
-private SodaBottleLabel(){
-  this.height=0;
+
+  private SodaBottleLabel(){
+  this.height=0.0;
 
 }
+  public SodaBottleLid(double height){
+    this.height = height;
+
+  }
+
+
 
 
 }
